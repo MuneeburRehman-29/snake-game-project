ed537e6a735653ea36666842016c8271666f9f97
umais <professional.umais@gmail.com>
2025-11-15 12:15:56 +0500

    added score system

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
+#include <iostream>
+#include <string>
 #include <raylib.h>
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
 
-int main() {
+int main()
+{
     const int screenWidth = 800;
     const int screenHeight = 900;
     int BallX = screenWidth / 2;
@@ -16,44 +19,64 @@ int main() {
     int foodX;
     int foodY;
     int foodRadius = 10;
+    int score = 0;
 
     InitWindow(screenWidth, screenHeight, "first game window by umais using raylib");
     SetTargetFPS(59);
     srand(time(NULL));
 
-    do {
+    do
+    {
         foodX = rand() % screenWidth;
         foodY = rand() % screenHeight;
     } while (foodX == BallX && foodY == BallY);
 
-    while (!WindowShouldClose()) {
-        if (IsKeyDown(KEY_RIGHT)) key = 'R';
-        if (IsKeyDown(KEY_LEFT)) key = 'L';
-        if (IsKeyDown(KEY_UP)) key = 'U';
-        if (IsKeyDown(KEY_DOWN)) key = 'D';
+    while (!WindowShouldClose())
+    {
+        if (IsKeyDown(KEY_RIGHT))
+            key = 'R';
+        if (IsKeyDown(KEY_LEFT))
+            key = 'L';
+        if (IsKeyDown(KEY_UP))
+            key = 'U';
+        if (IsKeyDown(KEY_DOWN))
+            key = 'D';
 
-        if (key == 'R') BallX += 5;
-        if (key == 'L') BallX -= 5;
-        if (key == 'U') BallY -= 5;
-        if (key == 'D') BallY += 5;
+        if (key == 'R')
+            BallX += 5;
+        if (key == 'L')
+            BallX -= 5;
+        if (key == 'U')
+            BallY -= 5;
+        if (key == 'D')
+            BallY += 5;
 
-        if (BallX > screenWidth) BallX = 0;
-        if (BallX < 0) BallX = screenWidth;
-        if (BallY > screenHeight) BallY = 0;
-        if (BallY < 0) BallY = screenHeight;
+        if (BallX > screenWidth)
+            BallX = 0;
+        if (BallX < 0)
+            BallX = screenWidth;
+        if (BallY > screenHeight)
+            BallY = 0;
+        if (BallY < 0)
+            BallY = screenHeight;
 
         float dx = BallX - foodX;
         float dy = BallY - foodY;
-        float distance = sqrt(dx*dx + dy*dy);
+        float distance = sqrt(dx * dx + dy * dy);
 
-        if (distance <= playerRadius + foodRadius) {
-            do {
+        if (distance <= playerRadius + foodRadius)
+        {
+            do
+            {
+                score++;
                 foodX = rand() % screenWidth;
                 foodY = rand() % screenHeight;
             } while (foodX == BallX && foodY == BallY);
         }
 
         BeginDrawing();
+        std::string scoreText = "Score: " + std::to_string(score);
+        DrawText(scoreText.c_str(), 10, 10, 20, white);
         ClearBackground(black);
         DrawCircle(foodX, foodY, foodRadius, red);
         DrawCircle(BallX, BallY, playerRadius, white);
